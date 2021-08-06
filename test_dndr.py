314ef72a56be6640a3f93cfdeb7fce12d2d7ef57
the-pythonist <68852419+the-pythonist@users.noreply.github.com>
2021-08-06 11:28:44 +0100

    Add files via upload

--- test_dndr.py
+++ test_dndr.py
@@ -3,14 +3,14 @@ import tkinter
 
 window = tkinter.Tk()
 
-button = tkinter.Button(text = "Test Widget")
+button = tkinter.Button(text="Test Widget")
 
-button.place(x=10,y=10,width=20,height=20)
+button.place(x=10, y=10, width=50, height=60)
 
-dndr_button = tkinter_dndr.DragDropResizeWidget(but)
-dndr_button.make_draggable_and_resizable() # if you want the widget to be both draggable and resizable
-dndr_button.make_draggable() # if you want the widget to only have drag and drop support
-dndr_button.make_resizable() # if you want the widget to only be resizable
+dndr_button = tkinter_dndr.DragDropResizeWidget(button)
+dndr_button.make_draggable()  # if you want the widget to only have drag and drop support
+dndr_button.make_resizable()  # if you want the widget to only be resizable
+dndr_button.make_draggable_and_resizable()  # if you want the widget to be both draggable and resizable
 
 window.mainloop()
 
